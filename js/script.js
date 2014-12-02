2824e443f680f5f274a454fca9124932b81aeae1
erw57 <stonewe123@gmail.com>
2014-12-01 22:59:58 -0500

    finish all the photo uploads and personal.html

--- js/script.js
+++ js/script.js
@@ -7,6 +7,14 @@
  *
  * @author wanger
  */
+ //this getQueryString() function comes from http://www.cnblogs.com/jiekk/archive/2011/06/28/2092444.html
+function getQueryString(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) 
+        return unescape(r[2]); 
+    return null;
+}
 
 function chkEmail(str) {   
     return str.search(/[\w\-]{1,}@[\w\-]{1,}\.[\w\-]{1,}/)==0?true:false 
@@ -80,9 +88,8 @@ $(document).ready(function(){
                     dataType: "json",
                     success: function(data){
                         if(data.flag==true)
-                            window.location.href='index.html';
+                            window.location.href='photo.html?uid='+$('#uid').val();
                         else{
-                            myApp.hidePleaseWait();
                             bootbox.alert(data.message);
                         }
                     }
@@ -122,14 +129,124 @@ $(document).ready(function(){
                     dataType: "json",
                     success: function(data){
                         if(data.flag==true)
-                            alert(data.flag);
-                            //window.location.href='index.html';
+                            window.location.href='personal.html';
                         else{
                             bootbox.alert(data.flag);
                         }
                     }
                 });
             }
+        });
+    }
+    
+    if(document.getElementById('viewProfile')){
+        $.ajax({
+            type: "GET",
+            url: "ViewProfile",
+            data: {},
+            dataType: "json",
+            success: function(data){
+                $('#uid').html(data.uid);
+                $('#email').html(data.email);
+                $('#fname').html(data.fname);
+                $('#lname').html(data.lname);
+                $('#password').html(data.password);
+                if(data.gender=='M')
+                    $('#gender').html("Male");
+                else
+                    $('#gender').html("Female");
+                $('#age').html(data.age);
+                $('#street').html(data.street);
+                $('#city').html(data.city);
+                $('#state').html(data.state);
+                $('#zip').html(data.zip);
+                $('img').attr('src',data.photosrc);
+            }
+        });
+        
+        $(document).on('click','#edit',function(){
+            window.location.href='edit.html';
+        });
+        $(document).on('click','#photochange',function(){
+            window.location.href='photochange.html?uid='+$('#uid').html();
+        });
+    }
+    
+    if(document.getElementById('photochange')){
+        var uid=getQueryString("uid");
+        $('#imgName').val(uid+"_photo");
+        $('#uid').val(uid);
+        $.ajax({
+            type: "GET",
+            url: "GetUserPhoto",
+            data: {uid:uid},
+            dataType: "json",
+            success: function(data){
+                //alert(data.photosrc);
+                $("img").attr("src",data.photosrc);
+            }
+        });
+        
+        $(document).on('click','#default',function(){
+            $("img").attr("src","images/photos/Face.svg");            
+        });
+        
+        $(document).on('click','#return',function(){
+            window.location.href='personal.html?uid='+uid;            
+        });
+        
+        $(document).on('click','#submit',function(){
+            var photo=$("img").attr("src");
+            $.ajax({
+                type: "GET",
+                url: "FinishUpdatePhoto",
+                data: {uid:uid,photosrc:photo},
+                dataType: "json",
+                success: function(data){
+                    //alert(data.photosrc);
+                    if(data.flag==true)
+                        window.location.href='personal.html?uid='+uid;
+                    else
+                        alert("false");
+                }
+            });
+        });
+    }
+    
+    if(document.getElementById('photo')){
+        var uid=getQueryString("uid");
+        $('#imgName').val(uid+"_photo");
+        $('#uid').val(uid);
+        $.ajax({
+            type: "GET",
+            url: "GetUserPhoto",
+            data: {uid:uid},
+            dataType: "json",
+            success: function(data){
+                //alert(data.photosrc);
+                $("img").attr("src",data.photosrc);
+            }
+        });
+        
+        $(document).on('click','#default',function(){
+            $("img").attr("src","images/photos/Face.svg");            
+        });
+        
+        $(document).on('click','#submit',function(){
+            var photo=$("img").attr("src");
+            $.ajax({
+                type: "GET",
+                url: "FinishUpdatePhoto",
+                data: {uid:uid,photosrc:photo},
+                dataType: "json",
+                success: function(data){
+                    //alert(data.photosrc);
+                    if(data.flag==true)
+                        window.location.href='index.html';
+                    else
+                        alert("false");
+                }
+            });
         });
     }
     
